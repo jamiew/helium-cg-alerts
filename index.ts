07c5e929315a4052d581bcca375ca67798be3974
Jamie Dubs <jamie@jamiedubs.com>
2022-08-03 10:14:14 -0400

    Messing around

--- index.ts
+++ index.ts
@@ -46,7 +46,7 @@ async function main() {
   let existingCg: Array<string> = [];
   if (existsSync(cgFile)) {
     existingCg = JSON.parse(readFileSync(cgFile, "utf-8"));
-    console.log(`existing CG:`, existingCg);
+    // console.log(`existing CG:`, existingCg);
   } else {
     console.log(`no ${cgFile} saved yet`);
   }
@@ -54,19 +54,29 @@ async function main() {
   // fetch current CG group
   const cg = await fetchConsensusGroup();
   const cgNames = cg.map((v) => v.name);
-  console.log(`currently in consensus:`, cgNames);
+  // console.log(`currently in consensus:`, cgNames);
 
   // check if any of our validators were in the previously-collected CG group
-  const inPrevConsensus = existingCg.filter((v) => validatorNames.includes(v));
-  console.log({ inPrevConsensus });
+  const inPrevConsensus = existingCg
+    // .filter((v) => validatorNames.includes(v));
+    .filter((v) => Math.random() > 0.5);
+  console.log(`inPrevConsensus:`, inPrevConsensus);
 
   // check if any of our validators are in current consensus group
-  const inConsensus = cg.filter((v) => validatorNames.includes(v.name));
-  console.log({ inConsensus });
+  const inConsensus = cg
+    // .filter((v) => validatorNames.includes(v.name))
+    .filter((v) => Math.random() > 0.5)
+    .map((v) => v.name);
+  console.log(`inConsensus:`, inConsensus);
 
-  // if yes, and wasn't before, send Joined CG
-  // if no, and WAS before, send Exited CG
-  // otherwise do nothing
+  // const combined = inPrevConsensus + inConsensus;
+  // console.log(`combined:`, combined);
+  // for (const val in combined) {
+  //   console.log(`checking ${val} ...`);
+  //   // if yes, and wasn't before, send Joined CG
+  //   // if no, and WAS before, send Exited CG
+  //   // otherwise do nothing
+  // }
 
   console.log(`saving current CG to ${cgFile} ...`);
   writeFileSync(cgFile, JSON.stringify(cgNames));
